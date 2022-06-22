7792ccd1f7564b68a19792e46c11f1085d22e8c1
Adrian Cañete <canete.adrian@gmail.com>
2022-06-22 16:17:38 -0300

    Version 2

--- src/navigation.js
+++ src/navigation.js
@@ -61,6 +61,13 @@ function categoriesPage() {
 	categoriesPreviewSection.classList.add("inactive");
 	genericSection.classList.remove("inactive");
 	movieDetailSection.classList.add("inactive");
+
+	const [_, categoryData] = location.hash.split("=");
+	const [categoryId, categoryName] = categoryData.split("-");
+
+	headerCategoryTitle.innerHTML = categoryName;
+
+	getMoviesByCategory(categoryId);
 }
 
 function movieDetailsPage() {
